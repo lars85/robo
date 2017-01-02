b393ce72cd45443cec4d40fc65a1e4e622e5f1ff
Lars Malach <lam@koempf.de>
2017-01-02 09:38:45 +0100

    [TASK] Use update instead of install

--- web/update.php
+++ web/update.php
@@ -1,6 +1,9 @@
 <?php
 
-if (empty($_GET['code']) || $_GET['code'] !== '4jCeZuJoNVkQCoFMoVBgptTQtvctpMkF' || empty($_POST)) {
+if (empty($_GET['code']) ||
+    !file_exists('../../.updateCode.txt') ||
+    trim(file_get_contents('../../.updateCode.txt')) !== $_GET['code']
+) {
     header('HTTP/1.0 404 Not Found');
     die();
 }
@@ -12,10 +15,15 @@ if (file_exists('update.lock')) {
 $commands = [
     'cd ..',
     'git pull',
-    'composer install',
+    'composer update --no-dev',
     'vendor/bin/robo phar:build'
 ];
 
 exec(implode(' && ', $commands), $output);
 
-mail('Lars@Malach.de', 'RoboUpdate', implode(PHP_EOL, $output));
+mail(
+    'Lars@Malach.de',
+    'RoboUpdate',
+    implode(PHP_EOL, $output) . PHP_EOL . PHP_EOL .
+    print_r(['$_POST' => $_POST, '$_SERVER' => $_SERVER], true)
+);
